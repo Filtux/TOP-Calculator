bd4d5cb6a6da531026b3755327a799fd77578da6
Joe <joe@joematthews.com.au>
2024-05-13 19:14:29 +0930

    working calculator with CSS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = '';
+let secondNum = '';
+let operator = null;
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -24,10 +24,51 @@ function operate(operator, a, b) {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
         case '/':
             return divide(a, b);
     }
-
 }
+
+function clearScreen() {
+    display.textContent = 0;
+    firstNum = '';
+    secondNum = '';
+}
+
+let display = document.querySelector('.calculator-display')
+
+document.querySelectorAll('.digit').forEach(button => {
+    button.addEventListener('click', function() {
+        if (operator === null) {
+            firstNum += button.textContent;
+            display.textContent = firstNum;
+        } else {
+            secondNum += button.textContent;
+            display.textContent = secondNum;
+        }
+    });
+});
+
+document.querySelectorAll('.operator').forEach(button => {
+    button.addEventListener('click', function() {
+        if (firstNum !== '') {
+            operator = button.textContent;
+        }
+    });
+});
+
+document.querySelector('.equals').addEventListener('click', function() {
+    if (firstNum !== '' && secondNum !== '' && operator) {
+        let result = operate(operator, firstNum, secondNum);
+        console.log(result)
+        console.log({operator},{firstNum},{secondNum})
+        display.textContent = result;
+        firstNum = result;
+        secondNum = '';
+        operator = null;
+    }
+})
+
+document.querySelector('.clear').addEventListener('click', clearScreen);
